a0c9153060cf0f151feb291a533ff34d3c110bd9
Jayson Galban <100999803+JeeSenpai@users.noreply.github.com>
2024-05-25 16:11:46 +0800

    second commit

---
+++ backend/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ToDoModule } from './to-do/to-do.module';
+@Module({
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.DATABASE_HOST,
+    port: parseInt(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    entities: [],
+    synchronize: true,
+  }), ToDoModule,
+],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
